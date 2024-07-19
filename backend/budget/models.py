b33d8551ba250e47a5ab3640807d470858a02ad9
Anuj Joshi <aj1097@usnh.edu>
2024-07-18 22:05:05 -0400

    Added DB model

--- backend/budget/models.py
+++ backend/budget/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.category}: {self.amount}"
